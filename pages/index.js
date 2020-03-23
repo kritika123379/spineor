dae507d8f71855d8ca58193e0b321be2a177a764
Jalaj Goel <jalajgoel45@gmail.com>
2020-03-24 00:35:26 +0530

    Architecture

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import ListProperty from '../components/listProperty';
 import fetch from 'node-fetch'
 import { fetcher } from '../shared/globalFunc';
 import Link from 'next/link';
+import SocialMeta from '../components/social-meta';
+import { SkipNavContent } from '@reach/skip-nav';
 
 export default (props) => {
   const isAmp = useAmp();
